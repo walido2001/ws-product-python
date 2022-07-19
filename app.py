340936f94e5fe75f87701d8187b99021cfe5da0e
Walid AlDari <walidald@my.yorku.ca>
2022-07-19 15:36:05 +0300

    Added datatable views

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+import json
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import sqlalchemy as sa
 
 # web app
@@ -9,10 +10,10 @@ app = Flask(__name__)
 # database engine
 SQL_URI = sa.engine.URL.create(
     drivername='postgresql',
-    username=os.getenv('PGUSER', ''),
-    password=os.getenv('PGPASSWORD', ''),
-    host=os.getenv('PGHOST', ''),
-    database=os.getenv('PGDATABASE', ''),
+    username=os.getenv('PGUSER', 'readonly'),
+    password=os.getenv('PGPASSWORD', 'w2UIO@#bg532!'),
+    host=os.getenv('PGHOST', 'work-samples-db.cx4wctygygyq.us-east-1.rds.amazonaws.com'),
+    database=os.getenv('PGDATABASE', 'work_samples'),
     port=os.getenv('PGPORT', 5432),
 )
 engine = sa.create_engine(SQL_URI)
@@ -32,6 +33,18 @@ def events_hourly():
         LIMIT 168;
     ''')
 
+@app.route('/events/hourly/dataTable')
+def events_hourly_dataTable():
+    data = query_helper_modified('''
+        SELECT date, hour, events
+        FROM public.hourly_events
+        ORDER BY date, hour
+        LIMIT 168;
+    ''')
+    keys = [key for key in data[0].keys()]
+    print(keys)
+
+    return render_template("dataTable.html", Title="Hourly Events DataTable", keys=keys, rows=data)
 
 @app.route('/events/daily')
 def events_daily():
@@ -43,6 +56,19 @@ def events_daily():
         LIMIT 7;
     ''')
 
+@app.route('/events/daily/dataTable')
+def events_daily_dataTable():
+    data = query_helper_modified('''
+        SELECT date, SUM(events) AS events
+        FROM public.hourly_events
+        GROUP BY date
+        ORDER BY date
+        LIMIT 7;
+    ''')
+    keys = [key for key in data[0].keys()]
+    print(keys)
+
+    return render_template("dataTable.html", Title="Daily Events DataTable", keys=keys, rows=data)
 
 @app.route('/stats/hourly')
 def stats_hourly():
@@ -53,6 +79,18 @@ def stats_hourly():
         LIMIT 168;
     ''')
 
+@app.route('/stats/hourly/dataTable')
+def stats_hourly_dataTable():
+    data = query_helper_modified('''
+        SELECT date, hour, impressions, clicks, revenue
+        FROM public.hourly_stats
+        ORDER BY date, hour
+        LIMIT 168;
+    ''')
+    keys = [key for key in data[0].keys()]
+    print(keys)
+
+    return render_template("dataTable.html", Title="Hourly Stats DataTable", keys=keys, rows=data)
 
 @app.route('/stats/daily')
 def stats_daily():
@@ -67,6 +105,22 @@ def stats_daily():
         LIMIT 7;
     ''')
 
+@app.route('/stats/daily/dataTable')
+def stats_daily_dataTable():
+    data = query_helper_modified('''
+        SELECT date,
+            SUM(impressions) AS impressions,
+            SUM(clicks) AS clicks,
+            SUM(revenue) AS revenue
+        FROM public.hourly_stats
+        GROUP BY date
+        ORDER BY date
+        LIMIT 7;
+    ''')
+    keys = [key for key in data[0].keys()]
+    print(keys)
+
+    return render_template("dataTable.html", Title="Daily Stats DataTable", keys=keys, rows=data)
 
 @app.route('/poi')
 def poi():
@@ -80,3 +134,8 @@ def query_helper(query):
     with engine.connect() as conn:
         result = conn.execute(query).fetchall()
         return jsonify([dict(row.items()) for row in result])
+
+def query_helper_modified(query):
+    with engine.connect() as conn:
+        result = conn.execute(query).fetchall()
+        return [dict(row.items()) for row in result]
